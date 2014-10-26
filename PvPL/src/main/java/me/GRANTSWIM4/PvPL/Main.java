09f08aca3f529be4062895a33ae87b8740ee60d3
Grant Baron <Grant@hqm.me>
2014-10-25 21:35:51 -0400

    Did some fixes

---
+++ PvPL/src/main/java/me/GRANTSWIM4/PvPL/Main.java
@@ -0,0 +1,33 @@
+package me.GRANTSWIM4.PvPL;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin
+{
+@Override
+public void onEnable()
+{
+	
+	{
+		// Welcome message
+		getLogger().info("Protecting your server form PvP logger since 2014!");
+
+		// Register events
+		getServer().getPluginManager().registerEvents(new PvPEvent(this), this);
+	}
+}
+	
+	
+		
+
+
+/**
+ * Bukkit disable.
+ */
+@Override
+public void onDisable()
+{
+	// Goodbye message
+	getLogger().info("It's been a honor protecting your server Bye!");			
+}
+}
---
+++ PvPL/src/main/java/me/GRANTSWIM4/PvPL/PvPEvent.java
@@ -0,0 +1,108 @@
+package me.GRANTSWIM4.PvPL;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PvPEvent implements Listener
+{
+	// Constants
+	private static final String IN_COMBAT = ChatColor.GOLD + "You're now in Combat!";
+	private static final String SAFE = ChatColor.GREEN + "You can now log out safely.";
+
+	// Variables
+	private List<String> antilog = new ArrayList<String>();
+	private Plugin plugin;
+
+	/**
+	 * Constructor
+	 */
+	PvPEvent(Plugin plugin)
+	{
+		this.plugin = plugin;
+	}
+
+	/**
+	 * Quit event.
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onAntiLogQuit(PlayerQuitEvent event)
+	{
+		Player p = event.getPlayer();
+		if(this.antilog.contains(p.getName()))
+		{
+		// If someone PvP logs goes here	
+			for (Player player: Bukkit.getServer().getOnlinePlayers()) {
+			    if (player.hasPermission("PVPN")) {
+			        player.sendMessage("You were sent a message");
+			    }
+			}	
+		}
+	}
+
+	/**
+	 * Damage by entity event.
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onAntiLogDmg(EntityDamageByEntityEvent event)
+	{
+		// Return if invalid
+		if(event.isCancelled() || !(event.getEntity() instanceof Player)) return;
+
+		Player target = (Player) event.getEntity();
+		Player damager;
+
+		// Arrow
+		if(event.getDamager() instanceof Arrow && ((Arrow) event.getDamager()).getShooter() instanceof Player)
+		{
+			damager = (Player) ((Arrow) event.getDamager()).getShooter();
+		}
+
+		// Player
+		else if(event.getDamager() instanceof Player)
+		{
+			damager = (Player) event.getDamager();
+		}
+
+		// Anything else
+		else return;
+
+		// Sanity Check
+		if(target.equals(damager)) return;
+
+		// Antilog
+		processPlayer(target);
+		processPlayer(damager);
+	}
+
+	private void processPlayer(final Player player)
+	{
+		// Antilog
+		if(!this.antilog.contains(player.getName()))
+		{
+			this.antilog.add(player.getName());
+			player.sendMessage(IN_COMBAT);
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable()
+			{
+				public void run()
+				{
+					if(antilog.contains(player.getName()))
+					{
+						antilog.remove(player.getName());
+						player.sendMessage(SAFE);
+					}
+				}
+			}, 1000L);
+		}
+	}
+}
